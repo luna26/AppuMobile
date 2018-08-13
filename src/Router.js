40bdb103074eb4e1705503abe3aa56dbc745d4af
Luna Gonzalez, Jose M <jose.m.luna.gonzalez@accenture.com>
2018-08-13 14:40:38 -0600

    init
    init

--- src/Router.js
+++ src/Router.js
@@ -24,10 +24,10 @@ const RouterComponent = () => {
                     tabs={true}
                     hideTabBar={true}
                 >
-                    {/* <Scene key="splash" component={Splash} hideNavBar={true} initial/> */}
+                    <Scene key="splash" component={Splash} hideNavBar={true} initial/>
                     <Scene key="home" component={Home} hideNavBar={true} />
                     <Scene key="carrers" component={Carrers} hideNavBar={true} />
-                    <Scene key="calc" component={Calculator} hideNavBar={true} initial/>
+                    <Scene key="calc" component={Calculator} hideNavBar={true}/>
                     <Scene key="info" component={Info} hideNavBar={true}/>
                 </Scene>
             </Scene>
