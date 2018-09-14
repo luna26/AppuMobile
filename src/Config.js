65732c557a6e997a58c8bafb9c377b51a7cf04b2
Luna Gonzalez, Jose M <jose.m.luna.gonzalez@accenture.com>
2018-09-14 11:04:55 -0600

    test

--- src/Config.js
+++ src/Config.js
@@ -1,1 +1,3 @@
-export const SERVER_DIR = 'http://34.213.97.133';
+// export const SERVER_DIR = 'http://34.213.97.133';
+// backend.ucemapp.com
+export const SERVER_DIR = 'http://backend.ucemapp.com';
